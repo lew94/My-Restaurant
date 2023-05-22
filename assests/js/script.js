be7941fca6cd194197fdcd54df44d9e4c15a336a
Lewis <lewishammond2@gmail.com>
2023-05-22 23:17:19 +0000

    added opening hours and styled in css, js tested and working

---
+++ assests/js/script.js
@@ -0,0 +1,8 @@
+//NavBar response
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
