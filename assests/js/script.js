4a906422694d1679acec98a7d7695ca598f13ae1
Lewis <lewishammond2@gmail.com>
2023-05-23 01:32:58 +0000

    carousel added to home, images added to new home folder in images file

--- assests/js/script.js
+++ assests/js/script.js
@@ -6,3 +6,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+//Home page Carousel
+$(document).ready(function () {
+  // Activate the Carousel
+  $("#carouselExample").carousel({
+    interval: 3000, // Set the interval for automatic rotation (in milliseconds)
+    pause: false, // Disable pausing on hover
+    wrap: true // Enable wrapping at the end of the Carousel
+  });
+});
